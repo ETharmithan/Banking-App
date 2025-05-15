b0967228880ffcd0f68d5c74c7cafee17ba71163
ETharmithan <tharmithan5@gmail.com>
2025-05-16 01:22:10 +0530

    Transaction History Menu Add

--- Bank_App.py
+++ Bank_App.py
@@ -74,16 +74,15 @@ def create_account():
     with open("Transaction_History.txt","a") as file2:
         file2.write(f"{customer_id_02} | {New_ID} | {user_initialbalnce} | Deposit | {user_initialbalnce}\n")
 
-#==============================================================================================
 #---------------------Money---------------------
 #Withdraw======================================================================================
-def Withdraw(customer_id_03):
+def Withdraw(account_id):
     with open("Accounts.txt","r") as file:
         lines=file.readlines()
     # input_account_no=input("Enter your account number: ") 
     change_data=[]
     for line in lines:
-        if customer_id_03 in line.strip().split(" | "):
+        if account_id in line.strip().split(" | "):
             datas=line.strip().split(" | ")
             print(f"Hi, {datas[2]}")
             print(f"Your current balance is {datas[4]}")
@@ -115,13 +114,13 @@ def Withdraw(customer_id_03):
             file2.writelines(change_data)
 
 #Deposit======================================================================================
-def Deposit(customer_id_03,):
+def Deposit(account_id):
     with open("Accounts.txt", "r") as file:
         lines=file.readlines()
     # account_number_input=input("Enter your account number: ") 
     Change_data=[]
     for line in lines:
-        if customer_id_03 in line.strip().split(" | "):
+        if account_id in line.strip().split(" | "):
             Datas=line.strip().split(" | ")
             print(f"Hi, {Datas[2]}")
             print(f"Your current balance is {Datas[4]}")
@@ -151,23 +150,49 @@ def Deposit(customer_id_03,):
 
 #Transaction History===========================================================================
 #Customer Transaction History==================================================================
-def Customer_Transaction_History(customer_id_03):
+def Customer_Transaction_History(account_id):
     with open("Transaction_History.txt","r") as file:
         lines=file.readlines()
     # enter_account_id=input("Enter your account ID: ")
-    customer_history=[]
     for line in lines:
-        if customer_id_03 in line.strip().split(" | "):
+        if account_id in line.strip().split(" | "):
             records=line.strip().split(" | ")
             print(f"{records[1]} | {records[2]} | {records[3]} | {records[4]}")
 
+#Admin Transaction History=====================================================================
+def Admin_Transaction_History():
+    with open("Transaction_History.txt", "r") as file:
+        lines=file.readlines()
+        for line in lines:
+            all_lines=line.strip().split(" | ")
+            print(f"{all_lines[0]} | {all_lines[1]} | {all_lines[2]} | {all_lines[3]} | {all_lines[4]}")
+
+#Transaction History===========================================================================
+def Transaction_History():
+    print("---------History---------")
+    print("1.All Transaction History")
+    print("2.Transaction History of One Customer")
+    while True:
+        try:
+            Choose=int(input("Choose 1  or 2: "))
+            if Choose>0 and Choose<3:
+                if Choose==1:
+                    Admin_Transaction_History()
+                else:
+                    account_id=input("Enter Customer Account ID: ")
+                    Customer_Transaction_History(account_id)
+            else:
+                print("Please enter only the positive number 1 or 2!")
+        except ValueError:
+            print("Please enter numbers only!")
+
 #Show Balance==================================================================================
-def Show_Balance(customer_id_03):
+def Show_Balance(account_id):
     with open("Accounts.txt", "r") as file:
         lines=file.readlines()
     # Enter_account_id=input("Enter your account ID: ")
     for line in lines:
-        if customer_id_03 in line.strip().split(" | "):
+        if account_id in line.strip().split(" | "):
             print(f"Hi, {line.strip().split(" | ")[2]}")
             print(f"Your balance is {line.strip().split(" | ")[-1]}.")
 
@@ -175,7 +200,6 @@ def Show_Balance(customer_id_03):
 def Customer_Menu(customer_id_03):
     with open("Accounts.txt", "r") as file:
         lines=file.readlines()
-
     for line in lines:
         if customer_id_03 in line.strip().split(" | "):
             while True:
@@ -190,13 +214,13 @@ def Customer_Menu(customer_id_03):
                     choose=int(input("Enter the number only (1 to 5): "))
                     if choose>0:
                         if choose==1:
-                            Deposit(customer_id_03)
+                            Deposit(account_id)
                         elif choose==2:
-                            Withdraw(customer_id_03)
+                            Withdraw(account_id)
                         elif choose==3:
-                            Show_Balance(customer_id_03)
+                            Show_Balance(account_id)
                         elif choose==4:
-                            Customer_Transaction_History(customer_id_03)
+                            Customer_Transaction_History(account_id)
                         elif choose==5:
                             print("Thank you for choosing us!")
                             break
@@ -227,17 +251,16 @@ def Admin_Menu():
                 elif choose==2:
                     create_account()
                 elif choose==3:
-                    customer_id_03=input("Enter the Customer ID: ")
-                    Deposit(customer_id_03)
+                    account_id=input("Enter the Account ID: ")
+                    Deposit(account_id)
                 elif choose==4:
-                    customer_id_03=input("Enter the Customer ID: ")
-                    Withdraw(customer_id_03)
+                    account_id=input("Enter the Account ID: ")
+                    Withdraw(account_id)
                 elif choose==5:
-                    customer_id_03=input("Enter the Customer ID: ")
-                    Customer_Transaction_History(customer_id_03)
+                    Transaction_History()
                 elif choose==6:
-                    customer_id_03=input("Enter the Customer ID: ")
-                    Show_Balance(customer_id_03)
+                    account_id=input("Enter the Account ID: ")
+                    Show_Balance(account_id)
                 # elif choose==7:
                 #     print("Going to Mainmenu👈")
                 #     break  
@@ -284,9 +307,9 @@ def users():
                     for line in lines:
                         datas=line.strip().split(" | ")
                         if datas[2]==customer_username and datas[3]==customer_password:
-                            customer_id_03=datas[0]
+                            account_id=datas[1]
                             print(f"{datas[2]} Login successfully.")
-                            Customer_Menu(customer_id_03)
+                            Customer_Menu(account_id)
                     else:
                         print("Invalid Username or Password!")
                 else:
